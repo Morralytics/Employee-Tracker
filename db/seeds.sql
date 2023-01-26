20a3294ab320fe2e0b7127ac1376f05f3355cec7
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-25 22:59:26 -0800

    Just adding basic notes to understand what links to what.

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,3 +12,9 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES (
     -- Need to impliment values for each employee
 )
+
+
+-- For notes:
+    -- The department id will be linked to the role's description of the department id
+    -- While the role id of the employees will link with each employee's role id
+    -- The manager id holds the reference to the manager of the current employee
