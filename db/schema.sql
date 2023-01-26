332a0749e6a399330056357493de184545494713
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-25 22:24:14 -0800

    Added tables based on what will be needed. Primary keys might be adjusted to refer to linking each table by reference.

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,22 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+)
+
+CREATE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+)
