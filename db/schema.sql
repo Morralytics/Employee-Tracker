5c5799ade1a423e94a06a4458c547d69e808ca9d
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-25 22:38:39 -0800

    Added role reference to department id.

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,10 @@ CREATE TABLE role (
     id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 )
 
 CREATE employee (
