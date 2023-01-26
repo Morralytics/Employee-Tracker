33677f3e384dbc05fcfd82cb762c43bc69c31d39
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-25 22:56:25 -0800

    Changed the department name variable and added basic table seeding to understand what all will be implimented in each.

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (department_name)
+VALUES(
+    -- Need to impliment department name
+)
+
+INSERT INTO role (title, salary, department_id)
+VALUES (
+    -- Need to impliment the values of each role
+)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES (
+    -- Need to impliment values for each employee
+)
