6650f2b8f62bc87e4aa12e82a5485eec76741bc8
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-28 15:40:49 -0800

    Changed file name to index.js since it will be a command line app. Added basic inquirer prompt structured with async/await promise.

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = async () {
+    const answers = await inquirer
+        .prompt([
+            {
+                
+            }
+        ])
+}
