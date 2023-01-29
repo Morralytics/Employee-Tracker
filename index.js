9521305b27b009be91d7242781203eb6ef2e53b5
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-28 17:38:31 -0800

    Added list of initial question with a switch case started for the case of 'Quit'. Working as intended.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const questions = async () {
+const questions = async () => {
     const answers = await inquirer
         .prompt([
             {
-                
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'userChoice',
+                choices: [
+                    'View all employees',
+                    'Add employee',
+                    'Update employee role',
+                    'View all roles',
+                    'Add role',
+                    'View all departments',
+                    'Add department',
+                    'Quit'
+                ]
             }
         ])
+
+        switch (answers) {
+            case 'Quit':
+                exit();
+                break;
+        
+            default:
+                break;
+        }
 }
+
+questions();
