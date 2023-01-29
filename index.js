147d1040a6c4bc158ae472a734eebb01de9c394b
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-28 18:12:33 -0800

    Added .env file with a .gitignore for connection setup and security.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
+
 const questions = async () => {
     const answers = await inquirer
         .prompt([
@@ -35,7 +37,7 @@ const questions = async () => {
 
     // Department
     viewAllDepartments = () => {
-        
+
     }
 }
 
